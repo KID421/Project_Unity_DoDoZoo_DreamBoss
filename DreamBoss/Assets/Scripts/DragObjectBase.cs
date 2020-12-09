7ec2f8be897576f254897604f632868baab36d40
KID <50666207+KID421@users.noreply.github.com>
2020-12-09 10:59:53 +0800

    整理拖拉物件基底

---
+++ DreamBoss/Assets/Scripts/DragObjectBase.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragObjectBase : MonoBehaviour, IDragHandler, IEndDragHandler
+{
+    /// <summary>
+    /// 座標元件
+    /// </summary>
+    protected RectTransform rect;
+    /// <summary>
+    /// 原始尺寸
+    /// </summary>
+    protected Vector2 posOriginal;
+    /// <summary>
+    /// 滑鼠與中心點位移
+    /// </summary>
+    protected Vector3 posOffset;
+    /// <summary>
+    /// 是否開始拖拉
+    /// </summary>
+    protected bool startDrag;
+    /// <summary>
+    /// 是否正確
+    /// </summary>
+    protected bool correct;
+    /// <summary>
+    /// 正確位置
+    /// </summary>
+    protected Vector2 posCorrect;
+    /// <summary>
+    /// 判定正確的距離
+    /// </summary>
+    protected float distance;
+    /// <summary>
+    /// 關卡基底
+    /// </summary>
+    protected LevelBase level;
+
+    /// <summary>
+    /// 編號
+    /// </summary>
+    public int index;
+
+    protected virtual void Awake()
+    {
+        level = FindObjectOfType<LevelBase>();
+        rect = GetComponent<RectTransform>();
+        posOriginal = rect.anchoredPosition;
+    }
+
+    public virtual void OnDrag(PointerEventData eventData)
+    {
+        if (correct) return;                                            // 如果本飾品已在正確位置就跳出
+
+        if (!startDrag)                                                 // 如果還沒拖拉
+        {
+            startDrag = true;                                           // 開始拖拉
+
+            Vector3 posEvent = new Vector3(eventData.position.x, eventData.position.y, 0);
+            posOffset = posEvent - transform.position;
+
+            StartDrag();
+        }
+
+        transform.position = eventData.position - new Vector2(posOffset.x, posOffset.y);
+    }
+
+    /// <summary>
+    /// 開始拖拉，只執行一次
+    /// </summary>
+    protected virtual void StartDrag()
+    {
+        level.ani.SetBool("移動中", true);
+    }
+
+    public virtual void OnEndDrag(PointerEventData eventData)
+    {
+        if (correct) return;                                                    // 如果本飾品已在正確位置就跳出
+
+        startDrag = false;                                                      // 還沒拖拉
+        level.ani.SetBool("移動中", false);
+
+        //判定距離正確位置 20 個單位 並且 此部位尚未拖拉至正確位置
+        if (Vector2.Distance(rect.anchoredPosition, posCorrect) < distance) PositionCorrect();
+        else PositionWrong();
+    }
+
+    protected virtual void PositionCorrect()
+    {
+        correct = true;                                                     // 已在正確位置
+        rect.anchoredPosition = posCorrect;                                 // 放置正確位置
+        StartCoroutine(level.Correct());                                    // 正確特效
+    }
+
+    protected virtual void PositionWrong()
+    {
+        rect.anchoredPosition = posOriginal;                // 回到原點
+        StartCoroutine(level.Wrong());                      // 錯誤特效
+    }
+}
---
+++ DreamBoss/Assets/Scripts/Lv11_DragObject.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Lv11_DragObject : DragObjectBase
+{
+    //[Header("編號"), Tooltip("所有飾品：0 藍色上衣, 1 黃色上衣, 2 水壺, 3 背包, 4 粉色鞋子, 5 咖啡鞋子, 6 頭帶, 7 帽子, 8 裙子, 9 褲子")]
+    //public int index;
+    [Header("部位：0 頭飾、1 上衣、2 飾品、3 褲子、4 鞋子")]
+    public int indexPart;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        posCorrect = Lv11_Stylist.instance.posCorrectAll[index];        // 取得正確位置
+        distance = Lv11_Stylist.instance.distance;
+    }
+
+    protected override void PositionCorrect()
+    {
+        if (!Lv11_Stylist.instance.chooseParts[indexPart])
+        {
+            Lv11_Stylist.instance.chooseParts[indexPart] = true;
+            base.PositionCorrect();
+        }
+        else
+        {
+            PositionWrong();
+        }
+    }
+}
